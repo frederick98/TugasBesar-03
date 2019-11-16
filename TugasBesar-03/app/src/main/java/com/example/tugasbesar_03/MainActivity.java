7b134e0865fb49af64d726c9519d9def2814472f
Reynaldi Irfan <reynaldiirfannn@gmail.com>
2019-11-16 16:08:00 +0700

    Auto stash before merge of "develop" and "origin/develop"

--- TugasBesar-03/app/src/main/java/com/example/tugasbesar_03/MainActivity.java
+++ TugasBesar-03/app/src/main/java/com/example/tugasbesar_03/MainActivity.java
@@ -12,10 +12,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
 
 import com.example.tugasbesar_03.ui.main.SectionsPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
+    GridView gridView;
+
+    String[] numWord = {"1","2"};
+
+    int[] numberImage ={R.drawable.IMG_0653,R.drawable.IMG_0653};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +36,17 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
+
+        gridView = findViewById(R.id.grid_view);
+
+        MainAdapter adapter = new MainAdapter(MainActivity.this)
+
+        gridView.setOnClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(),"You Clicked"+numWord[+position],
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- TugasBesar-03/app/src/main/java/com/example/tugasbesar_03/ViewHolder.java
+++
@@ -1,7 +0,0 @@
-package com.example.tugasbesar_03;
-
-import android.widget.TextView;
-
-public class ViewHolder {
-    TextView textView;
-}
