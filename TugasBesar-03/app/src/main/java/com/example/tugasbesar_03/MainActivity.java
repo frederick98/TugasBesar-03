cbe9bb2fe4dadda38bcfdc3f6ae8a942d6141069
Frederick <fritzfrederick51@gmail.com>
2019-11-21 01:57:37 +0700

    all project rework, basic view 25% done

--- TugasBesar-03/app/src/main/java/com/example/tugasbesar_03/MainActivity.java
+++ TugasBesar-03/app/src/main/java/com/example/tugasbesar_03/MainActivity.java
@@ -1,21 +1,48 @@
 package com.example.tugasbesar_03;
 
+import android.app.ProgressDialog;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+
+import android.net.Uri;
+
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.tabs.TabLayout;
+import android.speech.RecognizerIntent;
+/*
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.support.v7.widget.Toolbar;
 
-import androidx.viewpager.widget.ViewPager;
-import androidx.appcompat.app.AppCompatActivity;
+ */
+
+import android.text.TextUtils;
+
+
+import android.util.JsonReader;
+import android.util.JsonToken;
+import android.util.Log;
 
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Adapter;
+
 import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.android.volley.*;
+import com.google.gson.Gson;
 
 import com.example.tugasbesar_03.ui.main.SectionsPagerAdapter;
 
@@ -34,11 +61,12 @@ public class MainActivity extends AppCompatActivity {
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
 
 
-        gridView = findViewById(R.id.grid_view);
+        //gridView = findViewById(R.id.grid_view);
 
         MainAdapter adapter = new MainAdapter(MainActivity.this,numWord,numberImage);
-        gridView.setAdapter(adapter);
+        //gridView.setAdapter(adapter);
 
+        /*
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -46,5 +74,7 @@ public class MainActivity extends AppCompatActivity {
                         Toast.LENGTH_SHORT).show();
             }
         });
+        
+         */
     }
 }
